f483955fe79d314657f7a9293362168212dd9ab2
Adam Vesecký <vesecky.adam@gmail.com>
2019-03-22 02:29:33 +0100

    Flutter: add choices for practice

--- memorizer_flutter/memorizer/lib/pages/practice.dart
+++ memorizer_flutter/memorizer/lib/pages/practice.dart
@@ -7,6 +7,7 @@ class Practice extends StatefulWidget {
 
   final String title;
   List<CategoryItem> items;
+  int currentIndex = 0;
 
   @override
   _PracticeState createState() => new _PracticeState();
@@ -17,17 +18,111 @@ class _PracticeState extends State<Practice> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body:  new Swiper(
-        itemBuilder: (BuildContext context, int index){
-          return new Image.network(widget.items[index].imageUrls.first, fit: BoxFit.cover,);
-        },
-        itemCount: widget.items.length,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
-      ),
+      backgroundColor: Colors.black,
+      body:
+        OrientationBuilder(
+          builder: (context, orientation) {
+            return
+              Stack(
+               children: <Widget>[
+                  Swiper(itemBuilder: (BuildContext context, int index){
+                    return new Image.network(widget.items[index].imageUrls.first, fit: BoxFit.cover,);
+                  },
+                    itemCount: widget.items.length,
+                    pagination: new SwiperPagination(),
+                    control: new SwiperControl(),
+                    itemWidth: orientation == Orientation.landscape ? MediaQuery.of(context).size.width : MediaQuery.of(context).size.height,
+                    itemHeight: orientation == Orientation.landscape ? MediaQuery.of(context).size.height : MediaQuery.of(context).size.width,
+                    layout: SwiperLayout.TINDER,
+                    onIndexChanged: ((idx) {
+                      widget.currentIndex = idx;
+                      setState(() {
+                      });
+                    })
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: new SizedBox(
+                                height: 50,
+                                child: FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.zero),
+                                textColor: Colors.white,
+                                color: Colors.red,
+                                child: new Text(
+                                  widget.items[widget.currentIndex].name.getString("cs"),
+                                ),
+
+                                onPressed: () => {},
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: new SizedBox(
+                              height: 50,
+                              child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero),
+                                textColor: Colors.white,
+                                color: Colors.red,
+                                child: new Text(
+                                  widget.items[widget.currentIndex].name.getString("cs"),
+                                ),
+
+                                onPressed: () => {},
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: new SizedBox(
+                              height: 50,
+                              child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero),
+                                textColor: Colors.white,
+                                color: Colors.red,
+                                child: new Text(
+                                  widget.items[widget.currentIndex].name.getString("cs"),
+                                ),
+
+                                onPressed: () => {},
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: new SizedBox(
+                              height: 50,
+                              child: FlatButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero),
+                                textColor: Colors.white,
+                                color: Colors.red,
+                                child: new Text(
+                                  widget.items[widget.currentIndex].name.getString("cs"),
+                                ),
+
+                                onPressed: () => {},
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+               ],
+              );
+          },
+        )
     );
   }
 }
